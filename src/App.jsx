a7c34d55f23bcb8a2be436ffdb84fc06bdc0bd6f
Rahul Deshpande <rahuldeshpande@10-19-201-166.dynapool.wireless.nyu.edu>
2019-10-08 16:09:35 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import FileInput from './FileInput/FileInput';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FileInput/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FileInput/FileInput.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+// import SpellChecker from 'spellchecker';
+import * as spell from 'spell-checker-js';
+// const spell = require('spell-checker-js');
+spell.load('en');
+
+class FileInput extends Component {
+    constructor(props){
+        super(props);
+        let fileReader;
+    }
+
+    handleFileRead = (file) => {
+        let content = this.fileReader.result;
+        // checker.isMisspelled('content');
+        console.log(spell.check('Some text to check, blahblahblah, olololo'));
+    }
+
+    handleFileChosen = (file) => {
+        this.fileReader = new FileReader();
+        this.fileReader.onloadend = this.handleFileRead;
+        this.fileReader.readAsText(file);
+    }
+
+    render() {
+        return (
+            <div>
+                <input type = "file"
+                    id = "file"
+                    onChange = {e => this.handleFileChosen(e.target.files[0])}
+                />
+            </div>
+        );
+    }
+}
+
+export default FileInput;
